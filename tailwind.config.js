d3edcebf71f6b9fcc50fb847976deb1af35c0e7b
Paula Hightower <eurolife@gmail.com>
2021-08-04 17:28:09 +0100

    SCSS, typography, colors setup

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**.*.js'],
+  mode: 'jit',
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    colors: {
+      // common
+      white: '#FFF',
+      black: '#000',
+      // primary
+      brightblue: 'hsl(220, 98%, 61%)',
+      checkbackground: 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+      // light
+      vlgray: 'hsl(0, 0%, 98%)',
+      vlgrayblue: 'hsl(236, 33%, 92%)',
+      lgrayblue: 'hsl(233, 11%, 84%)',
+      dgrayblue: 'hsl(236, 9%, 61%)',
+      vdgrayblue: 'hsl(235, 19%, 35%)',
+      // dark
+      vdblue: 'hsl(235, 21%, 11%)',
+      vddesblue: 'hsl(235, 24%, 19%)',
+      lgrayblue: 'hsl(234, 39%, 85%)',
+      lgraybluehover: 'hsl(236, 33%, 92%)',
+      dgrayblue: 'hsl(234, 11%, 52%)',
+      vdgrayblue: 'hsl(233, 14%, 35%)',
+      vdgrayblue2: 'hsl(237, 14%, 26%)',
+    },
+    extend: {
+      fontFamily: {
+        'josefine': ['"Josefin Sans"']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
